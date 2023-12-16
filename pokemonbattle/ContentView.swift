752200c7bf36cefb2de328feb7b260a359f2da4a
Marcus Ziadé <marcus.ziade@ouraring.com>
2023-12-16 13:46:11 -0800

    Initial Commit

---
+++ pokemonbattle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pokemonbattle
+//
+//  Created by Marcus Ziadé on 16.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pokemonbattle/pokemonbattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonbattleApp.swift
+//  pokemonbattle
+//
+//  Created by Marcus Ziadé on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct pokemonbattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
